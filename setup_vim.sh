05fbbd9505ea1c9bec4ac2193005b842fc51ec94
Jordan Mata <jordanmata15@gmail.com>
2023-02-01 12:53:36 -0800

    Made script independent of where it's run.

--- setup_vim.sh
+++ setup_vim.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 USER_HOME=$(getent passwd $SUDO_USER | cut -d: -f6)
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 ## copy over the vimrc to the user's home directory
-cp $USER_HOME/config-files/.vimrc $USER_HOME
+cp ${SCRIPT_DIR}/.vimrc ${USER_HOME}
 
 ## Create the .vim folder at the root
-mkdir ~/.vim
-cd ~/.vim
+mkdir ${USER_HOME}/.vim
+cd ${USER_HOME}/.vim
 
 ## Download pathogen
-mkdir -p ~/.vim/autoload ~/.vim/bundle
-apt install curl
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+mkdir -p ${USER_HOME}/.vim/autoload ~/.vim/bundle
+curl -LSso ${USER_HOME}/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 ## Add dracula theme to the .vim folder
 git clone https://github.com/jordanmata15/vim
