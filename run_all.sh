9e1416696bceeb95492d7732b2015fe3397aafc1
Jordan Mata <jordanmata15@gmail.com>
2023-04-28 11:55:08 -0700

    Removed setup_GTest.sh

--- run_all.sh
+++ run_all.sh
@@ -3,4 +3,3 @@
 sudo chmod +x ./setup_env.sh && sudo ./setup_env.sh
 sudo chmod +x ./setup_packages.sh && sudo ./setup_packages.sh
 sudo chmod +x ./setup_vim.sh && sudo ./setup_vim.sh
-#sudo chmod +x ./setup_GTest.sh && sudo ./setup_GTest.sh
