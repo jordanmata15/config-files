89970b51a82c178888b9cabbf039eb7a163d136d
Jordan Mata <jordanmata15@gmail.com>
2023-04-28 11:54:11 -0700

    Add github info

--- setup_env.sh
+++ setup_env.sh
@@ -10,3 +10,7 @@ cp "${USER_HOME}/.bashrc" "${USER_HOME}/.bashrc.bkp_$(date +%Y%m%d_%H%M%S%Z)"
 # concatenate our .bashrc (add some newlines between existing and our new stuff)
 echo -e "\n\n"                  >> "${USER_HOME}/.bashrc"
 cat "${RC_FILE_DIR}/.bashrc"    >> "${USER_HOME}/.bashrc"
+
+# Set up my github account info and editor preference (I hate nano)
+echo -e "[user]\n\tname = Jordan Mata\n\temail = jordanmata15@gmail.com" >> ~/.gitconfig
+echo -e "[core]\n\teditor = vim" >> ~/.gitconfig
